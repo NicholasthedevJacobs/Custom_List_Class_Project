0a832d9f493247ca5c4ea97c2bd630e46e11ce0a
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-08-25 14:14:09 -0500

    Added new test method to check the boolean return value from the Remove method.

--- UnitTestProjectCustomListClass/RemoveFromListTest.cs
+++ UnitTestProjectCustomListClass/RemoveFromListTest.cs
@@ -167,9 +167,33 @@ namespace UnitTestProjectCustomListClass
             customObject.AddToList(value5);
             customObject.RemoveFromList(100);
 
-            //assert
+            //assert          
             actual = customObject.ListCount;
             Assert.AreEqual(actual, expected);
+        }
+        public void Remove_Nonexistant_Number_Check_Bool()
+        {
+            //arrange
+            CustomBuiltList<int> customObject = new CustomBuiltList<int>();
+            int value1 = 1;
+            int value2 = 2;
+            int value3 = 3;
+            int value4 = 4;
+            int value5 = 5;
+            int expected = 5;
+            bool expectedReturnValue = false;
+
+            //act
+            customObject.AddToList(value1);
+            customObject.AddToList(value2);
+            customObject.AddToList(value3);
+            customObject.AddToList(value4);
+            customObject.AddToList(value5);
+            bool returnValue = customObject.RemoveFromList(100);
+
+            //assert
+
+            Assert.AreEqual(returnValue, expectedReturnValue);
 
         }
     }
