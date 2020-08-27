a146095c3841d644cc50405008d143b7aafbe783
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-08-27 14:23:56 -0500

    Created a test ethod to check what happens if two lists are subtracted but one list has more values as well as multiple repeat values.

--- UnitTestProjectCustomListClass/OverloadSubtractionOperatior.cs
+++ UnitTestProjectCustomListClass/OverloadSubtractionOperatior.cs
@@ -88,6 +88,58 @@ namespace UnitTestProjectCustomListClass
             //assert
             Assert.AreEqual(actual, expected);
         }
+        [TestMethod]
+        public void Both_Methods_Are_Empty()
+        {
+            //assign
+            CustomBuiltList<int> listOne = new CustomBuiltList<int>();
+            CustomBuiltList<int> listTwo = new CustomBuiltList<int>();
+            CustomBuiltList<int> listThree = new CustomBuiltList<int>();
+           
+            string actual;
+            string expected = "";        
+
+            //act
+            listThree = listOne - listTwo;
+            actual = listThree.ToString();
+
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
+        [TestMethod]
+        public void Subtract_Two_Lists_One_Has_More_Values_And_Double_Values()
+        {
+            //assign
+            CustomBuiltList<int> listOne = new CustomBuiltList<int>();
+            CustomBuiltList<int> listTwo = new CustomBuiltList<int>();
+            CustomBuiltList<int> listThree = new CustomBuiltList<int>();
+            int value1 = 1;
+            int value2 = 2;
+            int value3 = 3;
+            int value4 = 1;
+            int value5 = 5;
+            int value6 = 6;
+            int value7 = 7;
+            int value8 = 5;
+            string actual;
+            string expected = "3";
+
+            listOne.AddToList(value1);
+            listOne.AddToList(value3);
+            listOne.AddToList(value5);
+            listTwo.AddToList(value2);
+            listTwo.AddToList(value4);
+            listTwo.AddToList(value6);
+            listTwo.AddToList(value7);
+            listTwo.AddToList(value8);
+
+            //act
+            listThree = listOne - listTwo;
+            actual = listThree.ToString();
+
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
 
     }
 }
