3dd2d0fafd330190c8106db28c580137ec5b1141
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-08-27 11:50:55 -0500

    Added a method to test if the addition override method will work if two lists have differing amounts of values.

--- UnitTestProjectCustomListClass/OverloadAdditionOperator.cs
+++ UnitTestProjectCustomListClass/OverloadAdditionOperator.cs
@@ -61,6 +61,34 @@ namespace UnitTestProjectCustomListClass
             //assert
             Assert.AreEqual(actual, expected);
         }
+        [TestMethod]
+        public void One_List_Has_More_Values_Than_The_Other()
+        {
+            //assign
+            CustomBuiltList<int> listOne = new CustomBuiltList<int>();
+            CustomBuiltList<int> listTwo = new CustomBuiltList<int>();
+            CustomBuiltList<int> listThree = new CustomBuiltList<int>();
+            int value1 = 1;
+            int value3 = 3;
+            int value5 = 5;
+            int value2 = 2;
+            int value4 = 4;
+            string actual;
+            string expected = "13524";
+
+            listOne.AddToList(value1);
+            listOne.AddToList(value3);
+            listOne.AddToList(value5);
+            listTwo.AddToList(value2);
+            listTwo.AddToList(value4);
+
+            //act
+            listThree = listOne + listTwo;
+            actual = listThree.ToString();
+
+            //assert
+            Assert.AreEqual(actual, expected);
+        }
 
     }
 }
