5be028c6b785769e30e508c6db9791cdbcbc3df9
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-08-27 09:32:34 -0500

    Created two test methods for the ToString override method.

--- UnitTestProjectCustomListClass/ToStringOverride.cs
+++ UnitTestProjectCustomListClass/ToStringOverride.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomListClassProject;
 
 namespace UnitTestProjectCustomListClass
 {
@@ -7,8 +8,41 @@ namespace UnitTestProjectCustomListClass
     public class ToStringOverride
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test_If_List_Is_Converted_To_String()
         {
+            //arrange
+            CustomBuiltList<int> customObject = new CustomBuiltList<int>();
+            int value1 = 1;
+            int value2 = 2;
+            int value3 = 3;
+            int value4 = 4;
+            string expected = "1234";
+            string actual;
+
+            //act
+            customObject.AddToList(value1);
+            customObject.AddToList(value2);
+            customObject.AddToList(value3);
+            customObject.AddToList(value4);
+            actual = customObject.ToString();
+
+            //assert
+            Assert.AreEqual(actual, expected);
+
+        }
+        [TestMethod]
+        public void Test_Return_If_List_Empty()
+        {
+            //arrange
+            CustomBuiltList<int> customObject = new CustomBuiltList<int>();
+            string expected = "No More Values.";
+            string actual;
+
+            //act
+            actual = customObject.ToString();
+
+            //assert
+            Assert.AreEqual(actual, expected);
         }
     }
 }
