b94a18d5dc25a5f3a7de73c9e4fdfb0ceccdcde7
NicholasthedevJacobs <jjacobs.nicholas@gmail.com>
2020-09-06 15:34:42 -0500

    Added a test to see if zipper works when both lists have more than five values, which means both lists must be extended from the starting capacity of four.

--- UnitTestProjectCustomListClass/ZipperUnitTests.cs
+++ UnitTestProjectCustomListClass/ZipperUnitTests.cs
@@ -8,7 +8,7 @@ namespace UnitTestProjectCustomListClass
     public class ZipperUnitTests
     {
         [TestMethod]
-        public void Test_Standard_Zipper()
+        public void Test_When_Lists_Have_Equal_Number_Of_Values()
         {
             //assign
             CustomBuiltList<int> listOne = new CustomBuiltList<int>();
@@ -112,6 +112,45 @@ namespace UnitTestProjectCustomListClass
             //assert
             Assert.AreEqual(actual, expected);
         }
+        [TestMethod]
+        public void Test_When_Both_Lists_Have_More_Than_Five_Values()
+        {
+            //assign
+            CustomBuiltList<int> listOne = new CustomBuiltList<int>();
+            CustomBuiltList<int> listTwo = new CustomBuiltList<int>();
+            CustomBuiltList<int> listThree = new CustomBuiltList<int>();
+            CustomBuiltList<int> listFour = new CustomBuiltList<int>();
+            int value1 = 1;
+            int value2 = 2;
+            int value3 = 3;
+            int value4 = 4;
+            int value5 = 5;
+            int value6 = 6;
+            int value7 = 7;
+            int value8 = 8;
+            int value9 = 9;
+            int value10 = 10;
+            string actual;
+            string expected = "12345678910";
 
+            listOne.AddToList(value1);
+            listOne.AddToList(value3);
+            listOne.AddToList(value5);
+            listOne.AddToList(value7);
+            listOne.AddToList(value9);
+            listTwo.AddToList(value2);
+            listTwo.AddToList(value4);
+            listTwo.AddToList(value6);
+            listTwo.AddToList(value8);
+            listTwo.AddToList(value10);
+
+            //act
+            listFour = listThree.Zipper(listOne, listTwo);
+            actual = listFour.ToString();
+
+            //assert
+            Assert.AreEqual(actual, expected);
+
+        }
     }
 }
